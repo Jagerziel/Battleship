89690981f09cb2684011c0720fe4ecda1fcaed2b
Jagerziel <fromnytosg@gmail.com>
2022-11-30 12:05:04 -0500

    Building out logic to prevent ship overlap

--- TheBrain.js
+++ TheBrain.js
@@ -12,15 +12,15 @@ let compAttributes = []
 
 //Assign attributes 
 let attributes = {
-  shipID: '',
-  shipLength: 0,
-  shipSunk: false,
-  shipHit: false,
-  shipHitCount: 0,
-  shipMiss: false,
-  noShip: true,
-  gridTurn: false,
-  showShip: false
+  shipID: '', //Type of Ship
+  shipLength: 0, //Length of Ship
+  shipSunk: false, //Ship Present or Blank Tile Default Value
+  shipHit: false, //Ship Hit
+  shipHitCount: 0, //Ship Hit Count to check against length
+  shipMiss: false, //Ship Miss count for shot total, log miss in square
+  noShip: true, //Ocean
+  gridTurn: false, //
+  showShip: false //Show Ship on Board
 }
 
 //Insert objects into arrays.  Both player and comp done in one array (though this can be broken out into two)
@@ -29,8 +29,8 @@ for (let i = 0; i < playerSquares.length; i++) {
   compAttributes.push({attributes})
 }
 
-console.log(playerAttributes) //Atribute Testing
-console.log(playerAttributes[3].attributes.shipHit) //Pull specific attribute Testing
+// console.log(playerAttributes) //Atribute Testing
+// console.log(playerAttributes[3].attributes.shipHit) //Pull specific attribute Testing
 
 //Ship Placement (Step 1) - Random Placement
 
@@ -44,49 +44,66 @@ let fleet = {
   frigate: 2
 }
 
-console.log(Object.keys(fleet).length)
+// console.log(Object.keys(fleet).length)
+// console.log(Object.entries(fleet))
 
+//Computer Ship Placement
 for (let i = 0; i < Object.keys(fleet).length; i++) {
+  //Randomize ship: vertical or horizontal
   if (Math.random() > 0.5) {
     pos = "Horizontal"
   } else {
     pos = "Vertical"
   }
-  console.log(pos)
 
+  //Ship Placement 
+  if (pos === "Horizontal") {
+    let shipPlacement = false
+    while (shipPlacement === false) {
+      let index = Math.floor(Math.random() * 100)
+      let indexCheck = index;
+      let freeSpaces = 0
+      for (let k = Object.values(fleet)[i]; k > 0; k--) {
+        if (compAttributes[index].attributes.noShip === true) {
+          freeSpaces++  
+        }
+        indexCheck -= 1
+      } 
 
-
-
-
-
+      if (index % 10 - Object.values(fleet)[i] + 1 >= 0) {
+        shipPlacement = true;
+        console.log(`Start Horizontal Placement at index ${index}`)
+        for (let j = Object.values(fleet)[i]; j > 0; j--) {
+          compAttributes[index].attributes.shipID = Object.keys(fleet)[i]
+          compAttributes[index].attributes.shipLength = Object.values(fleet)[i]
+          compAttributes[index].attributes.noShip = false
+          compSquares[index].innerHTML = `X${i}`
+          console.log(`X placed at index ${index}`)
+          console.log(`Properties changed to ${compAttributes[index].attributes}`)
+          index -= 1
+        }
+      }
+    }
+  } else {
+    let shipPlacement = false
+    while (shipPlacement === false) {
+      let index = Math.floor(Math.random() * 100)
+      if (index + (Object.values(fleet)[i] * 10) - 10 < 100) {
+        shipPlacement = true;
+        console.log(`Start Vertical Placement at index ${index}`)
+        for (let j = 0; j < Object.values(fleet)[i]; j++) {
+          compAttributes[index].attributes.shipID = Object.keys(fleet)[i]
+          compAttributes[index].attributes.shipLength = Object.values(fleet)[i]
+          compAttributes[index].attributes.noShip = false
+          compSquares[index].innerHTML = `Y${i}`
+          console.log(`Y placed at index ${index}`)
+          console.log(`Properties changed to ${compAttributes[index].attributes}`)
+          index += 10
+        }
+      }
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -102,5 +119,3 @@ for (let i = 0; i < playerSquares.length; i++) {
       console.log(`Clicked square ${i}`);
     })
 }
-
-
---
+++ jsTestingGrounds.js
@@ -0,0 +1,5 @@
+let i = 50
+let j = i;
+console.log(i)
+j+=5
+console.log(j)
