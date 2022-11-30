1caf0550c79bfb53269a22a1660da57d4b797475
Jagerziel <fromnytosg@gmail.com>
2022-11-29 19:59:18 -0500

    Assigned Player and Comp Attributes

--- TheBrain.js
+++ TheBrain.js
@@ -1,4 +1,37 @@
 
+//Assign Player and Comp to variables and create nodelist for each
+const playerBoard = document.querySelector('.playerBoard')
+const playerSquares = playerBoard.children
+
+const compBoard = document.querySelector('.compBoard')
+const compSquares = compBoard.children
+
+//Assign Objects to run concurrent with squares, which allows for selectable attributes
+let playerAttributes = []
+let compAttributes = []
+
+
+let attributes = {
+  shipID: '',
+  shipLength: 0,
+  shipSunk: false,
+  shipHit: false,
+  shipHitCount: 0,
+  shipMiss: false,
+  noShip: true,
+  gridTurn: false,
+  showShip: false
+}
+
+//Insert objects into arrays.  Both player and comp done in one array (though this can be broken out into two)
+for (let i = 0; i < playerSquares.length; i++) {
+  playerAttributes.push({attributes})
+  compAttributes.push({attributes})
+}
+
+console.log(playerAttributes)
+
+
 
 
 
