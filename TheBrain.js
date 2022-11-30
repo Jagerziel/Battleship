78a1146fe39a887239fa95ad8f24dbb7bf1062e3
Jagerziel <fromnytosg@gmail.com>
2022-11-30 14:03:32 -0500

    working through ship placement

--- TheBrain.js
+++ TheBrain.js
@@ -12,25 +12,30 @@ let compAttributes = []
 
 //Assign attributes 
 let attributes = {
+  noShip: true, //Ocean
   shipID: '', //Type of Ship
   shipLength: 0, //Length of Ship
   shipSunk: false, //Ship Present or Blank Tile Default Value
   shipHit: false, //Ship Hit
   shipHitCount: 0, //Ship Hit Count to check against length
   shipMiss: false, //Ship Miss count for shot total, log miss in square
-  noShip: true, //Ocean
   gridTurn: false, //
   showShip: false //Show Ship on Board
 }
 
 //Insert objects into arrays.  Both player and comp done in one array (though this can be broken out into two)
 for (let i = 0; i < playerSquares.length; i++) {
-  playerAttributes.push({attributes})
-  compAttributes.push({attributes})
+  playerAttributes.push(attributes)
+  compAttributes.push(attributes)
 }
 
 // console.log(playerAttributes) //Atribute Testing
 // console.log(playerAttributes[3].attributes.shipHit) //Pull specific attribute Testing
+// abc = 50
+// for (let i = 0; i < 3; i++) {
+  console.log(`CHECKING ATTRIBUTE ${compAttributes[29].noShip}`) 
+//   abc--
+// }
 
 //Ship Placement (Step 1) - Random Placement
 
@@ -61,29 +66,41 @@ for (let i = 0; i < Object.keys(fleet).length; i++) {
     let shipPlacement = false
     while (shipPlacement === false) {
       let index = Math.floor(Math.random() * 100)
-      let indexCheck = index;
-      let freeSpaces = 0
-      for (let k = Object.values(fleet)[i]; k > 0; k--) {
-        if (compAttributes[index].attributes.noShip === true) {
-          freeSpaces++  
-        }
-        indexCheck -= 1
-      } 
-
       if (index % 10 - Object.values(fleet)[i] + 1 >= 0) {
         shipPlacement = true;
         console.log(`Start Horizontal Placement at index ${index}`)
-        for (let j = Object.values(fleet)[i]; j > 0; j--) {
-          compAttributes[index].attributes.shipID = Object.keys(fleet)[i]
-          compAttributes[index].attributes.shipLength = Object.values(fleet)[i]
-          compAttributes[index].attributes.noShip = false
-          compSquares[index].innerHTML = `X${i}`
-          console.log(`X placed at index ${index}`)
-          console.log(`Properties changed to ${compAttributes[index].attributes}`)
-          index -= 1
+        
+
+
+        let indexCheck = index
+        console.log(`index is ${index} and indexCheck is ${indexCheck}`)
+        let freeSpaces = 0
+        for (let k = Object.values(fleet)[i]; k > 0; k--) {
+          console.log(`noShip is equal to ${compAttributes[indexCheck].noShip}`)
+          if (compAttributes[indexCheck].noShip === true) {
+            freeSpaces++  
+          }
+          console.log(`IndexCheck is ${indexCheck} and free spaces is ${freeSpaces}`)
+          indexCheck -= 1
+        } 
+        if (Object.values(fleet)[i] === freeSpaces) {
+          console.log("SHIP PLACED")
+          for (let j = Object.values(fleet)[i]; j > 0; j--) {
+            compAttributes[index].shipID = Object.keys(fleet)[i]
+            compAttributes[index].shipLength = Object.values(fleet)[i]
+            // compAttributes[index].noShip = false
+            compSquares[index].innerHTML = `X${i}`
+            console.log(`X placed at index ${index}`)
+            console.log(`Properties changed to ${compAttributes[index]}`)
+            index -= 1
+          }
+        } else {
+          console.log("SHIP NOT PLACED")
         }
+        freeSpaces = 0;
       }
     }
+    console.log(compAttributes)
   } else {
     let shipPlacement = false
     while (shipPlacement === false) {
@@ -92,12 +109,12 @@ for (let i = 0; i < Object.keys(fleet).length; i++) {
         shipPlacement = true;
         console.log(`Start Vertical Placement at index ${index}`)
         for (let j = 0; j < Object.values(fleet)[i]; j++) {
-          compAttributes[index].attributes.shipID = Object.keys(fleet)[i]
-          compAttributes[index].attributes.shipLength = Object.values(fleet)[i]
-          compAttributes[index].attributes.noShip = false
+          compAttributes[index].shipID = Object.keys(fleet)[i]
+          compAttributes[index].shipLength = Object.values(fleet)[i]
+          // compAttributes[index].noShip = false
           compSquares[index].innerHTML = `Y${i}`
           console.log(`Y placed at index ${index}`)
-          console.log(`Properties changed to ${compAttributes[index].attributes}`)
+          console.log(`Properties changed to ${compAttributes[index]}`)
           index += 10
         }
       }
@@ -106,7 +123,7 @@ for (let i = 0; i < Object.keys(fleet).length; i++) {
 }
 
 
-
+// console.log(`FINAL CHECK ${compAttributes[50].attributes.noShip}`)
 
 
 
